5f8284fb7cb34841fe0186cf239c4a161a845bc0
root <you@example.com>
2024-09-03 15:09:36 +0000

    The last digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
 
-number = random.randint(-10000, 10000)
+num = random.randint(-10000, 10000)
 last_digit = abs(num) % 10
 
-if number < 0:
+if num < 0:
     last_digit = -last_digit
 
 if last_digit > 5:
