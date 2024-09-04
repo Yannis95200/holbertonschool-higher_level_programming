48334a55698775c41d2b7e907b193ab66805a3cf
Your Name <you@example.com>
2024-09-04 14:06:42 +0000

    Write a function that checks for lowercase character.

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,5 @@
 def islower(c):
-    for x in word:
-        if x.islower():
-            return True
-        else:
-            return False
+    if c >= 'a' and c <= 'z':
+        return True
+    else:
+        return False
