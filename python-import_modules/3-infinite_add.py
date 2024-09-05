e5115e535abd8625eaf74a4e8813b213fc4f556f
Yannis95200 <ranguin.yannis98@gmail.com>
2024-09-06 01:36:02 +0200

    corrig

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,6 +4,9 @@ import sys
 if __name__ == "__main__":
     arguments = sys.argv[1:]
 
-    total = sum(int(arg) for arg in arguments)
+    total = 0
+
+    for arg in arguments:
+        total += int(arg)
 
     print(total)
