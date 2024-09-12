8c59432c2c653b38ab8fa8665d2ce5f284cc9cd7
Yannis95200 <ranguin.yannis98@gmail.com>
2024-09-12 17:35:54 +0200

    corrige

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -16,9 +16,9 @@ def print_square(size):
     """
     if type(size) not in [int]:
         raise TypeError("size must be an integer")
-    if type(size) not in [float]:
-        raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
+    if type(size) not in [float]:
+        raise TypeError("size must be an integer")
     for index in range(size):
         print("#" * size)
