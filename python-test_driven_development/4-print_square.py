ffb02860919e365742289f8752f3025a761796cf
Yannis95200 <ranguin.yannis98@gmail.com>
2024-09-12 16:51:19 +0200

    commentaire

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
+"""
+This function prints a square using the '#' character.
+"""
 
 def print_square(size):
+    """
+    Prints a square with the size defined by `size` parameter.
 
+    Parameters:
+    size (int): The size of the square (both width and height).
+
+    Raises:
+    TypeError: If `size` is not an integer.
+    ValueError: If `size` is less than 0.
+    """
     if type(size) not in [int]:
         raise TypeError("size must be an integer")
     if size < 0:
