4eb2c92332847c7689bebc5c32ed2f23ae55afde
Yannis95200 <ranguin.yannis98@gmail.com>
2024-09-16 13:49:08 +0200

    Corriger

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,22 @@
-class Square:
-            def __init__(self,size):
-                    self.__size = size
+#!/usr/bin/python3
+"""
+Defines a class `Square`.
+"""
 
+
+class Square:
+    """
+    A class used to represent a Square.
+
+    Attributes:
+        __size (int): The size of the square's side (private).
+    """
+
+    def __init__(self, size):
+        """
+        Initializes the square with a given size.
+
+        Args:
+            size (int): The size of the square's side.
+        """
+        self.__size = size
