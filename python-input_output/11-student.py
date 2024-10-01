81693b481a80f2901094b261a42a29a6279a7184
Yannis95200 <ranguin.yannis98@gmail.com>
2024-10-01 17:25:01 +0200

    Write a class Student that defines a student by: (based on 10-student.py)

---
+++ python-input_output/11-student.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Module that defines a Student class.
+"""
+
+
+class Student:
+    """
+    Represents a student with a first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of the Student instance.
+
+        If attrs is provided, only the attributes specified in attrs
+        are included in the returned dictionary.
+
+        Args:
+            attrs (list, optional): A list of attribute names to include
+            in the dictionary representation. Defaults to None.
+
+        Returns:
+            dict: A dictionary containing the student's attributes.
+        """
+        if attrs is None:
+            return self.__dict__
+
+        new_dict = {}
+        for index in attrs:
+            if index in self.__dict__:
+                new_dict[index] = self.__dict__[index]
+        return new_dict
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance with the values
+        from the provided dictionary.
+
+        Args:
+            json (dict): A dictionary containing attribute names and values
+            to update the Student instance.
+        """
+        for key, value in json.items():
+            self.__dict__[key] = value
