fc23fdbea610dffa73dd0aa19715aff5bd042df3
Yannis95200 <ranguin.yannis98@gmail.com>
2024-10-01 12:04:00 +0200

    Write a function that returns an object (Python data structure) represented by a JSON string:

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Converts a JSON string to a Python object.
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Converts a JSON string to a Python object.
+
+    Args:
+        my_str (str): The JSON string to convert.
+
+    Returns:
+        object: The Python object represented by the JSON string.
+    """
+    return json.loads(my_str)
