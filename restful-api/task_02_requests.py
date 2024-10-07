55be1fcd6d002bc104f49d3593452e44aa2dd835
Yannis95200 <ranguin.yannis98@gmail.com>
2024-10-07 16:50:44 +0200

    corriger pycodestyle

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,6 +1,7 @@
 import requests
 import csv
 
+
 def fetch_and_print_posts():
     reponses = requests.get("https://jsonplaceholder.typicode.com/posts")
     print("Status code: {}".format(reponses.status_code))
@@ -9,6 +10,7 @@ def fetch_and_print_posts():
         post = reponses.json()
         for index in post:
             print(index['title'])
+
 
 def fetch_and_save_posts():
     reponses = requests.get("https://jsonplaceholder.typicode.com/posts")
