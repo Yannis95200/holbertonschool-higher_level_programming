70210400b23d956f81ea1abd5899bc3459120478
Yannis95200 <ranguin.yannis98@gmail.com>
2024-10-15 12:16:47 +0200

    corriger

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -39,14 +39,14 @@ def index():
 
 @app.route('/login', methods=['POST'])
 def login():
-    username = request.json.get("username", None)
-    password = request.json.get("password", None)
-    if username != "user1" or password != "password":
-        return jsonify({"msg": "Bad username or password"}), 401
+    username = request.json.get("username")
+    password = request.json.get("password")
+    user = users.get(username)
+    if user and check_password_hash(user['password'], password):
+        access_token = create_access_token(identity={"username": username, "role": user['role']})
+        return jsonify(access_token=access_token)
     else:
-        access_token = create_access_token(identity="user1", fresh=True)
-        refresh_token = create_refresh_token(identity="user1")
-        return jsonify(access_token=access_token, refresh_token=refresh_token)
+        return jsonify({"error": "Invalid credentials"}), 401
 
 
 @app.route('/jwt-protected', methods=['GET'])
