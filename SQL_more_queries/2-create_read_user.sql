441cf6fb30f5cdaa5a6bf8000524e6af58ffed2d
Yannis95200 <ranguin.yannis98@gmail.com>
2024-10-29 19:15:11 +0100

    Write a script that creates the database hbtn_0d_2 and the user user_0d_2.

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,5 +4,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Creates 'user_0d_2' on 'localhost' with the password 'user_0d_2_pwd' if they don't already exist
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
--- Grants SELECT privilege on all tables in the 'hbtn_0d_2' database to 'user_0d_2' on 'localhost'
+-- Grants SELECT privilege on all tables in the 'hbtn_0d_2' database to 'user_0d_2' on 'localhost
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
