2e5562d666d3152988a4d9fa63f6a5b12f6ad585
Yannis95200 <ranguin.yannis98@gmail.com>
2024-10-30 14:46:20 +0100

    CORRECTION

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- Selects 'id' and 'name' from 'cities' and 'name' from 'states' by joining the two tables on 'state_id'
 -- Orders the results by 'cities.id' in ascending order
-SELECT cities.id, cities.name states.name FROM cities
+SELECT cities.id, cities.name states.name
+FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
