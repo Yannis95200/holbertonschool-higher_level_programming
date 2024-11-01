683506ddca7954034969661e8438f3a337649563
Yannis95200 <ranguin.yannis98@gmail.com>
2024-11-01 01:51:28 +0100

    write a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+"""Script to connect to a MySQL database and retrieve states by exact name.
+Uses command-line arguments for authentication and state name input."""
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3],
+    )
+
+    state_name = sys.argv[4]
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY id ASC", (state_name,))
+
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    db.close()
